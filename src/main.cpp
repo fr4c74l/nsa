deb793fe5562825a936d7ff5cc29975e2636e760
Lucas Clemente Vella <lvella@gmail.com>
2013-11-30 02:42:52 -0200

    Drawing something on screen with OGRE.

--- src/main.cpp
+++ src/main.cpp
@@ -3,26 +3,49 @@
 #include <Ogre.h>
 #include <OgrePlugin.h>
 
+class Updater:
+	public Ogre::FrameListener
+{
+public:
+	Updater(Ogre::SceneNode *cube):
+		mCube(cube)
+	{}
+
+	bool frameStarted(const Ogre::FrameEvent &evt)
+	{
+		auto angle = Ogre::Radian(Ogre::Math::UnitRandom() * 0.1);
+		auto rand_dir = Ogre::Vector3::UNIT_X.randomDeviant(angle, Ogre::Vector3::UNIT_Y);
+		auto rot = Ogre::Vector3::UNIT_X.getRotationTo(rand_dir);
+		mCube->rotate(rot);
+		return true;
+	}
+private:
+	Ogre::SceneNode *mCube;
+};
+
 int main()
 {
 	Ogre::Root renderer("", "", "renderer.log");
 
+	// Load plugins
 	{
 		// A list of required plugins
 		Ogre::StringVector required_plugins;
 		required_plugins.push_back("GL RenderSystem");
-		required_plugins.push_back("Octree & Terrain Scene Manager");
+		required_plugins.push_back("Octree Scene Manager");
 
 		// List of plugins to load
 		Ogre::StringVector plugins_to_load;
-		plugins_to_load.push_back("RenderSystem_GL");
-		plugins_to_load.push_back("Plugin_OctreeSceneManager");
+		plugins_to_load.push_back("/usr/lib/x86_64-linux-gnu/OGRE-1.8.0/RenderSystem_GL");
+		plugins_to_load.push_back("/usr/lib/x86_64-linux-gnu/OGRE-1.8.0/Plugin_OctreeSceneManager");
 
 		for(auto& plugin_name: plugins_to_load) {
 			renderer.loadPlugin(plugin_name);
 		}
 		std::unordered_set<Ogre::String> installed_plugins;
+		std::cout << "Installed plugins:\n";
 		for(auto& plugin: renderer.getInstalledPlugins()) {
+			std::cout << "  - " << plugin->getName() << "\n";
 			installed_plugins.insert(plugin->getName());
 		}
 
@@ -34,13 +57,21 @@ int main()
 		}
 	}
 
-	if(!renderer.restoreConfig() && !renderer.showConfigDialog())
+	// Configure OpenGL RenderSystem
 	{
-		return 1;
+		Ogre::RenderSystem* rs = renderer.getRenderSystemByName("OpenGL Rendering Subsystem");
+		assert(rs->getName() == "OpenGL Rendering Subsystem");
+		// configure our RenderSystem
+		rs->setConfigOption("Full Screen", "No");
+		rs->setConfigOption("VSync", "No");
+		rs->setConfigOption("Video Mode", "800 x 600 @ 32-bit");
+	 
+		renderer.setRenderSystem(rs);
 	}
 
 	auto window = renderer.initialise(true, "No Such Arrocha");
-	auto sceneManager = renderer.createSceneManager("Octree & Terrain Scene Manager");
+
+	auto sceneManager = renderer.createSceneManager("OctreeSceneManager");
 
 	auto camera = sceneManager->createCamera("PlayerCam");
 
@@ -59,9 +90,11 @@ int main()
 	cube_node->attachObject(
 		sceneManager->createEntity("cube", "Prefab_Cube")
 	);
+	cube_node->setScale(Ogre::Vector3(0.02, 0.02, 0.02));
 
 	sceneManager->setAmbientLight(Ogre::ColourValue(0.5, 0.5, 0.5));
 
+	renderer.addFrameListener(new Updater(cube_node));
 	renderer.startRendering();
 
 	return 0;
